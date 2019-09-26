d99939a6d24c45c891604b08a817c578fe60c751
vincent <vaarcilal@eafit.edu.co>
2019-09-26 14:28:29 -0500

    fibcod_iter

---
+++ maraton2019/fibcod/fib_iter.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+list<int> cadena;
+
+int main()
+{
+    int b;
+    int state = 1;
+    while (cin >> b) {
+        state = 1;
+        list<int> cadena;
+        if (b == 0)
+            return 0;
+        for (int i = 0; i < b; ++i) {
+            int bi;
+            cin >> bi;
+            for (int j = 0; j < bi; j++) {
+                cadena.push_back(state);
+            }
+            if (state == 0)
+                state = 1;
+            else
+                state = 0;
+        }
+        long long n_2 = 1, n_1 = 2;
+        long long total = 0;
+
+        list<int>::reverse_iterator it = cadena.rbegin();
+        if (*it++ == 1)
+            total += n_2;
+        if (*it++ == 1)
+            total += n_1;
+
+        long long tmp;
+        for (; it != cadena.rend(); ++it){
+            tmp = n_2 + n_1;
+            tmp %= 524288;
+            if (*it == 1) {
+                total += tmp;
+                total %= 524288;
+            }
+            n_2 = n_1;
+            n_1 = tmp;
+        }
+        cout << total << endl;
+    }
+}
--- maraton2019/fibcod/main.cpp
+++ maraton2019/fibcod/main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
-
 struct Matrix {
     long mat[2][2];
 };
@@ -23,39 +21,17 @@ Matrix matPow(Matrix base, int p)
     Matrix ans;
     int i, j;
     for (i = 0; i < 2; i++)
-        for (j = 0; j < 2; j++){
+        for (j = 0; j < 2; j++)
             ans.mat[i][j] = (i == j);
-        }
 
     while (p) {
-        if (p & 1){
-            cout << (p & 1) << endl;
+        if (p & 1) 
             ans = matMul(ans, base);
-        }
+       
         base = matMul(base, base);
         p >>= 1;
     }
     return ans;
-}
-
-void sol(unsigned int t, int c, vector<int> cad)
-{
-    Matrix fib;
-    fib.mat[0][1] = 1;
-    fib.mat[0][0] = 1;
-    fib.mat[1][0] = 1;
-    fib.mat[1][1] = 0;
-    Matrix base_mat = fib;
-    for (int k = cad.size() - 1; k >= 0; --k) {
-        int value = cad[k];
-        if (value != 0) {
-            Matrix asl = matPow(base_mat, c + 2);
-            t += asl.mat[0][1];
-        }
-
-        ++c;
-    }
-    cout << t << endl;
 }
 
 int main()
@@ -67,25 +43,6 @@ int main()
     fib.mat[1][1] = 0;
     Matrix base_mat = fib;
 
-    int b;
-    int state = 1;
-    while (cin >> b) {
-        state = 1;
-        vector<int> cadena;
-        cadena.reserve(400000);
-        if (b == 0)
-            return 0;
-        for (int i = 0; i < b; ++i) {
-            int bi;
-            cin >> bi;
-            for (int j = 0; j < bi; j++) {
-                cadena.push_back(state);
-            }
-            if (state == 0)
-                state = 1;
-            else
-                state = 0;
-        }
-        sol(0, 0, cadena);
-    }
+    Matrix ans = matPow(base_mat, 6);
+    cout << ans.mat[0][1] << endl;
 }
